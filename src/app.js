bb3ec3d240d5744f5f8305750263c7c51e3cdbe7
Sofia Vanessa <105688383+sofiat25@users.noreply.github.com>
2023-05-21 18:51:29 +0000

    Domain G

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let articles = ["the", "an", "some", "another", "any"];
+  let adjectives = ["awesome", "cool", "fantastic", "thrilling", "funny"];
+  let nouns = ["website", "blog", "webapp", "spot", "hub"];
+  let tids = [
+    ".com",
+    ".de",
+    ".org",
+    ".space",
+    ".rocks",
+    ".barcelona",
+    ".cat",
+    ".dog"
+  ];
+
+  let domaInNames = [];
+
+  for (let article of articles) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let tid of tids) {
+          domaInNames.push(`${article}${adjective}${noun}${tid}`);
+        }
+      }
+    }
+  }
+
+  document.querySelector("#generate").onclick = () => {
+    document.querySelector("#domain").innerHTML = `<h1>${
+      domaInNames[Math.floor(Math.random() * domaInNames.length)]
+    } </h1>`;
+  };
 };
